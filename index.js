abd4d458db18a0b7d0b0514e1dc918aaeb453d8d
Pati Bueno <patibuennodev@gmail.com>
2021-11-16 21:53:33 -0300

    configurações da arquitetura

--- index.js
+++ index.js
@@ -5,30 +5,8 @@ const port = 8080;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Minha primeira requisição");
+  res.send("Dasa Educa - Artigos");
 });
 
-app.get("/segunda-req", (req, res) => {
-  res.send("Minha segunda requisição");
-});
-
-app.get("/com-parametros", (req, res) => {
-  res.send("Com parâmetros funciona " + req.query.nome);
-});
-
-app.post("/meu-primeiro-post", (req, res) => {
-  console.log(req.body);
-  res.send("Meu primeiro post funciona");
-});
-
-app.put("/meu-primeiro-put/:id", (req, res) => {
-  console.log(req.body, req.params.id);
-  res.send("Meu primeiro put funciona");
-});
-
-app.delete("/meu-primeiro-delete/:id", (req, res) => {
-  console.log(req.params)
-  res.send("Meu primeiro delete funciona");
-});
 
 app.listen(port, () => console.log(`Ouvindo a porta: ${port}`));
